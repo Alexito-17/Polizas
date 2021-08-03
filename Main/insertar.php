615e3f49d4ef7e32ba4699e64bdfb7ba0d379aa7
Alexito-17 <73041509+Alexito-17@users.noreply.github.com>
2021-08-03 18:00:52 -0500

    arreglo

--- Main/insertar.php
+++ Main/insertar.php
@@ -1,8 +1,8 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "yes", "bd_pruebas")
-$nombre = $_POST [];
-$apellido = $_POST [];
-$fecha = $_POST [];
+$conexion = mysqli_connect("localhost", "root", "yes", "bd_pruebas");
+$nombre = $_POST ["nombre"];
+$apellido = $_POST ["apellido"];
+$fecha = $_POST ["fecha"];
 
 if($_FILES["archivos"])
 {
@@ -12,14 +12,14 @@ $ruta = "archivos/". $nombre_final;
 $subir_archivo = move_uploaded_file($_FILES["archivo"]["tmp_nom"], $ruta);
     if($subir_archivo)
     {
-        $insertarSQL = "INSER INTO informes (nombre, apellidos, fecha, archivo) VALUES ('$nombre', '$apellido', '$fecha', '$ruta')";
+        $insertarSQL = "INSERT INTO informes(nombre, apellidos, fecha, archivo) VALUES('$nombre', '$apellido', '$fecha', '$ruta')";
         $resultado = mysqli_query($conexion, $insertarSQL);
             if($resultado)
             {
-                echo "<script>alert ('Se ha enviado su informe'); window.location='IndexPrueba.html'</script>";
+                echo "<script>alert('el informe se ha enviado correctamente'); window.location= 'indexPrueba.html'</script>";
             }else
             {
-                print("Errormessage: %s\n", mysqli_error($conexion));
+                printf("Errormessage: %s\n", mysqli_error($conexion));
             }   
     }
     else
